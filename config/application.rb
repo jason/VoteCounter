cbc971d404456ac5d07e8bbe8d24dde05d4c9e06
Jason Wong <technique@gmail.com>
2013-02-26 13:34:09 -0800

    change application.rb config for heroku

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module VoteCounter
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Make Heroku Happy https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting
+    config.assets.initialize_on_precompile = false
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
